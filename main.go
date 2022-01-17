a61e16d3352e849f51864cb60ca657cf826261d2
Andrii Maliuta <quadr988@gmail.com>
2022-01-17 17:41:56 +0200

    get page

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"confluence-rest-golang/serv"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("test")
+	CONF_URL := "http://localhost:7150"
+	pageServ := serv.PageService{}
+	page := pageServ.GetPage(CONF_URL, 65603)
+	fmt.Println(page)
 }
--- serv/service.go
+++ serv/service.go
@@ -2,6 +2,7 @@ package serv
 
 import (
 	"confluence-rest-golang/models"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,10 +13,28 @@ import (
 type PageService struct {
 }
 
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
+	req.Header.Add("Authorization", "Basic "+basicAuth("admin", "admin"))
+	return nil
+}
+
 func (s PageService) GetPage(url string, id int64) models.Content {
 
+	client := &http.Client{
+		CheckRedirect: redirectPolicyFunc,
+	}
+
 	reqUrl := fmt.Sprintf("%s/rest/api/content/%d", url, id)
-	resp, err := http.Get(reqUrl)
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	//req.SetBasicAuth("admin", "admin")
+	//resp, err := http.Get(reqUrl)
+	req.Header.Add("Authorization", "Basic "+basicAuth("admin", "admin"))
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Panicln(err)
 	}
